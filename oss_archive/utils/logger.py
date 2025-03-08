d9721aeb1c1c813901efe327557d14e1bb8ddeac
m-shrief <m.shrief.dev@proton.me>
2025-03-09 01:15:19 +0200

    Added structlog and configured it with basic configuration for usage during development and production.

---
+++ oss_archive/utils/logger.py
@@ -0,0 +1,35 @@
+import logging
+import structlog
+###
+from oss_archive.config import ENV
+
+proccessors_configs: []
+
+if ENV == "dev": # Prettier logs for development
+    proccessors_configs = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        structlog.dev.set_exc_info,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+        structlog.dev.ConsoleRenderer(),
+    ]
+else: # JSON format, if it's in production
+    proccessors_configs = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.dev.set_exc_info,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+        structlog.processors.JSONRenderer(),
+    ]
+
+structlog.configure(
+    processors=proccessors_configs,
+    wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+    context_class=dict,
+    logger_factory=structlog.PrintLoggerFactory(),
+    cache_logger_on_first_use=False
+)
+
+logger: logging.Logger = structlog.get_logger()
