238610ad33ce5b78c29e33e40e15712cb38b40f7
m-shrief <m.shrief.dev@proton.me>
2025-04-19 09:09:25 +0200

    Refactoring compress() and decompress() function to return a boolean to indicate if the operation succeded or not.

--- oss_archive/utils/compress.py
+++ oss_archive/utils/compress.py
@@ -2,20 +2,29 @@ import os
 import tarfile
 ### 
 from oss_archive.config import ARCHIVE_BASE_PATH
+from oss_archive.utils.logger import logger
 
-async def compress(dir_name: str):
-    path = f"{ARCHIVE_BASE_PATH}/{dir_name}"
-    with tarfile.open(f"{path}.tar", 'w:gz') as tar_file:
-        tar_file.add(path, arcname=os.path.basename(path))
-    
-    return
-    
-
-async def decompress(file_name: str):
-    path = f"{ARCHIVE_BASE_PATH}/{file_name}"
+async def compress(dir_name: str) -> bool:
     try:
+        path = f"{ARCHIVE_BASE_PATH}/{dir_name}"
+        with tarfile.open(f"{path}.tar", 'w:gz') as tar_file:
+            tar_file.add(path, arcname=os.path.basename(path))
+        
+        return True
+    except Exception as e:
+        logger.error("Error compressing the directory", error=e) # pyright:ignore[reportCallIssue]
+        return False
+    
+
+async def decompress(file_name: str) -> bool:
+    try:
+        path = f"{ARCHIVE_BASE_PATH}/{file_name}"
+        if ARCHIVE_BASE_PATH is None:
+            raise Exception("Archive path is not defined")
+
         with tarfile.open(path, "r") as tar:
             tar.extractall(path=ARCHIVE_BASE_PATH)
-        print("Tar file decompressed successfully.")
+        return True
     except Exception as e:
-        print(f"Error decompressing tar file: {e}")
+        logger.error("Error decompressing tar file" , error=e) # pyright:ignore[reportCallIssue]
+        return False
