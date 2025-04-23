2114dcd33e5900558aa88b9b2f451c9d7b289d71
m-shrief <m.shrief.dev@proton.me>
2025-04-23 08:46:37 +0200

    Used meta_lists's JSONSchema instead of the old schema.

--- oss_archive/components/meta_lists/json.py
+++ oss_archive/components/meta_lists/json.py
@@ -1,19 +1,17 @@
 import json
 from pydantic import ValidationError
 import os
-# import mimetypes
-from typing import List 
 ###
 from oss_archive.config import JSON_META_LISTS_PATH
 from oss_archive.utils.logger import logger
-from oss_archive.components.meta_lists.schema import MetaList
+from oss_archive.schemas.meta_list import JSONSchema 
 
 
-def get_meta_lists()->List[MetaList]:
-    meta_lists : List[MetaList] = []
+def get_meta_lists()->list[JSONSchema]:
+    meta_lists : list[JSONSchema] = []
 
     for file in get_meta_lists_files():
-        meta_list: MetaList | None = get_meta_list_from_file(file)
+        meta_list: JSONSchema | None = get_meta_list_from_file(file)
         if meta_list is None:
             continue
         meta_lists.append(meta_list)
@@ -21,16 +19,16 @@ def get_meta_lists()->List[MetaList]:
     return meta_lists
 
 
-def get_meta_lists_files() -> List[str]:
+def get_meta_lists_files() -> list[str]:
     meta_lists = [file for file in os.listdir(JSON_META_LISTS_PATH) if file.endswith('.json')]
     return meta_lists
 
-def get_meta_list_from_file(meta_list_file: str)-> MetaList | None:
-    """meta_list_file naming convention is {meta_list.key}.json"""
+def get_meta_list_from_file(meta_list_file: str)-> JSONSchema | None:
+    """meta_list_file naming convention is {key}.json"""
     try:
         with open(f"{JSON_META_LISTS_PATH}/{meta_list_file}", 'r') as file:
             data = json.load(file)
-            return MetaList(**data)
+            return JSONSchema(**data)
     except json.JSONDecodeError as e:
         logger.error(f"Couldn't read {meta_list_file}", error=e)
         return None
@@ -41,22 +39,10 @@ def get_meta_list_from_file(meta_list_file: str)-> MetaList | None:
         logger.error(f"Unknown error in {meta_list_file}", error=e)
         return None
 
-def write_meta_list_file(meta_list: MetaList)->bool:
+def write_meta_list_file(meta_list: JSONSchema)->bool:
     try:
         with open(f"{JSON_META_LISTS_PATH}/{meta_list.key}.json", "w") as file:
             json.dump(meta_list.model_dump(), file)
             return True
     except Exception:
         return False
-
-# def get_files_as_dict() -> Dict[str,str]:
-#     files_dict = {}
-#     for f in os.listdir(OSS_LIST):
-#         file_path = os.path.join(OSS_LIST, f)
-#         if os.path.isfile(file_path):
-#             file_type, _ = mimetypes.guess_type(f)
-#             if file_type:
-#                 files_dict[f] = file_type
-#             else:
-#                 files_dict[f] = "Unknown"
-#     return files_dict
