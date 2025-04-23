cac73bd62e294dc44748abaa6491867ab80bba94
m-shrief <m.shrief.dev@proton.me>
2025-04-23 08:44:44 +0200

    Added general schemas in schemas folder.

---
+++ oss_archive/schemas/general.py
@@ -0,0 +1,44 @@
+
+from pydantic import Field, BaseModel
+from datetime import datetime
+from typing import Annotated
+from uuid import UUID
+from enum import Enum
+
+### General Fields
+
+class ActionsType(str, Enum):
+    DownloadAll = "download_all"
+    DownloadOnly = "download_only"
+    DownloadExcept = "download_except"
+
+
+class OwnerType(str, Enum):
+    Individual = "Individual"
+    Organization = "Organization"
+
+class IdField(BaseModel):
+    id: UUID
+
+class PriorityField(BaseModel):
+    priority: Annotated[int, Field(ge=1, le=10)]
+
+class ReviewedField(BaseModel):
+    reviewed:  Annotated[bool, Field(default=False)]
+
+class CreatedAtField(BaseModel):
+    created_at: datetime
+
+class UpdatedAtField(BaseModel):
+    updated_at: datetime
+
+
+# General relations' fields
+class MetaListKeyField(BaseModel):
+    meta_list_key: Annotated[str, Field(max_length=256, examples=["ai"])]
+
+class MetaItemIdField(BaseModel):
+    meta_item_id: UUID
+
+class LicenseKeyField(BaseModel):
+    license_key: Annotated[str, Field(max_length=256, examples=["mit"])]
