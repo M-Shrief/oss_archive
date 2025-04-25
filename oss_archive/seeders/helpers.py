6a0c15f7287f6677e7145f8feae174db76ecc20e
m-shrief <m.shrief.dev@proton.me>
2025-04-25 15:01:44 +0300

    Added should_apply_action_on_oss() as a helper function to decide whether to seed the OSS or not, added get_all_meta_list().

--- oss_archive/seeders/helpers.py
+++ oss_archive/seeders/helpers.py
@@ -1,11 +1,23 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from sqlalchemy import exc 
-from uuid import UUID
+from typing import Any
 from collections.abc import Sequence
 ### 
 from oss_archive.utils.logger import logger
 from oss_archive.database.models import MetaList, MetaItem, OSSoftware, License
+from oss_archive.schemas.general import ActionsType
+
+def get_all_meta_lists(db: Session) -> Sequence[MetaList] | None:
+    try:
+        stmt = select(MetaList)
+        res = db.scalars(stmt)
+        meta_lists = res.all()
+        return meta_lists
+    except Exception as e:
+        logger.error("Unknown error getting all meta lists", error=e)
+        return None
+
 
 def get_all_meta_items(db: Session) -> Sequence[MetaItem] | None:
     try:
@@ -14,8 +26,29 @@ def get_all_meta_items(db: Session) -> Sequence[MetaItem] | None:
         meta_items = res.all()
         return meta_items
     except Exception as e:
-        logger.error("Unknown error getting all meta_items", error=e)
+        logger.error("Unknown error getting all meta items", error=e)
         return None
+
+
+def should_apply_action_on_oss(meta_item: MetaItem, repo_dict: dict[str, Any])-> bool:
+    """Decide should we apply the meta_item.actions on the OSS or not."""
+    should_download = False
+    repo_name: str | Any = repo_dict.get("name")
+    if repo_name is None:
+        return False
+
+    # Filter seeded repos depending on meta_item.actions && meta_item.actions_on
+    match meta_item.actions:
+        case ActionsType.DownloadAll: # Get all repos without filtering
+            should_download = True
+        case ActionsType.DownloadOnly:
+            if repo_name in meta_item.actions_on:
+                should_download = True
+        case ActionsType.DownloadExcept:
+            if repo_name not in meta_item.actions_on:
+                should_download = True
+    
+    return should_download
 
 
 def does_meta_list_exists(meta_list_key: str, db: Session) -> bool:
