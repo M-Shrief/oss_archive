d525ec6eba89b7d2e756dace6d13ef9635ca21f5
m-shrief <m.shrief.dev@proton.me>
2025-04-26 11:14:14 +0300

    Used Licenses schemas as a response_model for it's routers.

--- oss_archive/components/licenses/router.py
+++ oss_archive/components/licenses/router.py
@@ -1,21 +1,22 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from typing import List
+from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, exc
 ###
-from oss_archive.database.index import get_db
+from oss_archive.database.index import get_async_db
 from oss_archive.database.models import License as LicenseModel
-from oss_archive.components.licenses.schema import License as LicenseSchema 
+
+from oss_archive.schemas.license import FullSchema
 
 router = APIRouter(tags=["Licenses"])
 
 @router.get(
     "/licenses",
     status_code=status.HTTP_200_OK,
-    response_model=List[LicenseSchema],
+    response_model=list[FullSchema],
     response_model_exclude_none=True
 )
-async def get_licenses(db: AsyncSession = Depends(get_db)):
+async def get_licenses(db: Annotated[AsyncSession, Depends(get_async_db)]):
     try:
         stmt = select(LicenseModel)
         res = await db.scalars(statement=stmt)
@@ -28,10 +29,10 @@ async def get_licenses(db: AsyncSession = Depends(get_db)):
 @router.get(
     "/licenses/{key}",
     status_code=status.HTTP_200_OK,
-    response_model=LicenseSchema,
+    response_model=FullSchema,
     response_model_exclude_none=True
 )
-async def get_license_by_key(key: str, db: AsyncSession = Depends(get_db)):
+async def get_license_by_key(key: str, db: Annotated[AsyncSession, Depends(get_async_db)]):
     try:
         stmt = select(LicenseModel).where(LicenseModel.key == key)
         res = await db.scalars(statement=stmt)
