8ef14592cd9e02a9d128d9ff41cd6f8dbf12c43a
m-shrief <m.shrief.dev@proton.me>
2025-04-26 19:34:02 +0300

    Refactored the code to differentiate between Archive and Compressed-Archive paths.

--- oss_archive/utils/tarfile.py
+++ oss_archive/utils/tarfile.py
@@ -1,18 +1,19 @@
 import os
 import tarfile
 ### 
-from oss_archive.config import COMPRESSED_ARCHIVE_BASE_PATH
+from oss_archive.config import ARCHIVE_BASE_PATH, COMPRESSED_ARCHIVE_BASE_PATH
 from oss_archive.utils.logger import logger
 
 async def compress(oss_fullname: str) -> bool:
     """compress OSS directory"""
     try:
-        if COMPRESSED_ARCHIVE_BASE_PATH is None:
+        if ARCHIVE_BASE_PATH is None or COMPRESSED_ARCHIVE_BASE_PATH is None:
             raise Exception("Compressed Archive's path is not defined")
             
-        path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}"
-        with tarfile.open(f"{path}.tar", 'w:gz') as tar_file:
-            tar_file.add(path, arcname=os.path.basename(path))
+        archive_path = f"{ARCHIVE_BASE_PATH}/{oss_fullname}"
+        compressed_archive_path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}.tar"
+        with tarfile.open(compressed_archive_path, 'w:gz') as tar_file:
+            tar_file.add(archive_path, arcname=os.path.basename(archive_path))
         
         return True
     except Exception as e:
@@ -23,12 +24,14 @@ async def compress(oss_fullname: str) -> bool:
 async def decompress(oss_fullname: str) -> bool:
     """decompress OSS's tarfile"""
     try:
-        path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}.tar"
-        if COMPRESSED_ARCHIVE_BASE_PATH is None:
+        if ARCHIVE_BASE_PATH is None or COMPRESSED_ARCHIVE_BASE_PATH is None:
             raise Exception("Compressed Archive's path is not defined")
 
-        with tarfile.open(path, "r") as tar:
-            tar.extractall(path=COMPRESSED_ARCHIVE_BASE_PATH)
+        archive_path = f"{ARCHIVE_BASE_PATH}/{oss_fullname}"
+        compressed_archive_path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}.tar"
+
+        with tarfile.open(compressed_archive_path, "r") as tar:
+            tar.extractall(archive_path)
         return True
     except Exception as e:
         logger.error("Error decompressing tar file" , error=e) 
