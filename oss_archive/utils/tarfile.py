fdd3cb9bdd74274c2cc7adfce0606dae728de043
m-shrief <m.shrief.dev@proton.me>
2025-04-28 17:30:47 +0300

    Used paths utils in tarfile to remove duplicate code.

--- oss_archive/utils/tarfile.py
+++ oss_archive/utils/tarfile.py
@@ -1,17 +1,14 @@
 import os
 import tarfile
 ### 
-from oss_archive.config import ARCHIVE_BASE_PATH, COMPRESSED_ARCHIVE_BASE_PATH
+from oss_archive.utils import paths
 from oss_archive.utils.logger import logger
 
 async def compress(oss_fullname: str) -> bool:
     """compress OSS directory"""
     try:
-        if ARCHIVE_BASE_PATH is None or COMPRESSED_ARCHIVE_BASE_PATH is None:
-            raise Exception("Compressed Archive's path is not defined")
-            
-        archive_path = f"{ARCHIVE_BASE_PATH}/{oss_fullname}"
-        compressed_archive_path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}.tar"
+        archive_path = paths.get_oss_archive_path(oss_fullname)
+        compressed_archive_path = paths.get_oss_compressed_archive_path(oss_fullname)
         with tarfile.open(compressed_archive_path, 'w:gz') as tar_file:
             tar_file.add(archive_path, arcname=os.path.basename(archive_path))
         
@@ -24,11 +21,8 @@ async def compress(oss_fullname: str) -> bool:
 async def decompress(oss_fullname: str) -> bool:
     """decompress OSS's tarfile"""
     try:
-        if ARCHIVE_BASE_PATH is None or COMPRESSED_ARCHIVE_BASE_PATH is None:
-            raise Exception("Compressed Archive's path is not defined")
-
-        archive_path = f"{ARCHIVE_BASE_PATH}/{oss_fullname}"
-        compressed_archive_path = f"{COMPRESSED_ARCHIVE_BASE_PATH}/{oss_fullname}.tar"
+        archive_path = paths.get_oss_archive_path(oss_fullname)
+        compressed_archive_path = paths.get_oss_compressed_archive_path(oss_fullname)
 
         with tarfile.open(compressed_archive_path, "r") as tar:
             tar.extractall(archive_path)
