44ff9e2bf4f841a1fab703418ec3eef082d28522
m-shrief <m.shrief.dev@proton.me>
2025-07-04 17:11:11 +0300

    FIX: imported based_header to use it in migrate requests.

--- oss_archive/components/forgejo/tasks.py
+++ oss_archive/components/forgejo/tasks.py
@@ -4,7 +4,7 @@ from oss_archive.config import Forgejo
 from oss_archive.utils import httpx
 from oss_archive.utils.logger import logger
 from oss_archive.components.forgejo.schema import ForgejoRepo, MigrateRepoReqBody
-
+from oss_archive.components.forgejo.shared import base_headers
 
 
 async def migrate_repo_task(body: MigrateRepoReqBody):
@@ -14,6 +14,7 @@ async def migrate_repo_task(body: MigrateRepoReqBody):
         base_url=Forgejo.get("base_url") or "",
         endpoint="/repos/migrate",
         body=body.model_dump(),
+        headers=base_headers,
         timeout=Timeout(timeout=500.0, connect=100.0)
         )
 
