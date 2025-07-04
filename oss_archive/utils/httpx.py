64c0351ebc198ea0518cfe87848d9105c978c48e
m-shrief <m.shrief.dev@proton.me>
2025-07-04 15:53:26 +0300

    Added Functions to use httpx to make Sync/Async GET requests.

--- oss_archive/utils/httpx.py
+++ oss_archive/utils/httpx.py
@@ -1,33 +1,21 @@
 """httpx config module to standardize our requests, adding retries, error handling and other features."""
-from typing import TypedDict
+from typing import TypedDict, Any
 from datetime import timedelta
-
-
-from httpx import URL, Client, Cookies, Headers, Response, Timeout
+from httpx import URL, Client, Cookies, Headers, Response, QueryParams, AsyncClient, Timeout
 from httpx import AsyncHTTPTransport, HTTPTransport
-# HTTPX's errors
-# from httpx import RequestError, NetworkError, HTTPStatusError, ConnectError, ReadError
-# Get Request params
-# from httpx import URL, Client, Cookies, Headers, Response
+from httpx import RequestError, NetworkError, HTTPStatusError, ConnectError, ReadError
 ###
-# from oss_archive.utils.logger import logger
-
-
-### Suggestion
-# We can make a factory class to initiate every time and get the an instance we want.
+from oss_archive.utils.logger import logger
 
 ### Retries
 # We can use tenacity(https://tenacity.readthedocs.io/en/latest/) to configure more complex logic
 SYNC_TRANSPORT = HTTPTransport(retries=3)
 ASYNC_TRANSPORT = AsyncHTTPTransport(retries=3)
 
-DEFAULT_TIMEOUT = Timeout(20.0, connect=10.0)
+DEFAULT_TIMEOUT = Timeout(5.0, connect=5.0)
 
+DEFAULT_HEADERS = Headers(headers={"Content-Type": "application/json"})
 
-# client = Client(
-#     transport=SYNC_TRANSPORT,
-#     timeout=DEFAULT_TIMEOUT
-#     )
 
 class ResponseMetadata(TypedDict):
     url: URL
@@ -43,26 +31,46 @@ def get_response_metadata(res: Response) -> ResponseMetadata:
 
     return ResponseMetadata(url=url, headers=headers, cookies=cookies, time_elapsed=time_elapsed)
 
-# def sync_get(url: URL | str, *, params: QueryParamTypes | None=None, headers: HeaderTypes | None=None, cookies: CookieTypes | None=None, auth: AuthTypes | UseClientDefault | None=USE_CLIENT_DEFAULT, follow_redirects: bool | UseClientDefault=USE_CLIENT_DEFAULT, timeout: TimeoutTypes | UseClientDefault=USE_CLIENT_DEFAULT, extensions: RequestExtensions | None=None)->Response | None:
-#     try:
-#         with Client(transport=SYNC_TRANSPORT) as client:
-#             response = client.get(
-#                 url=url,
-#                 params=params,
-#                 headers=headers,
-#                 cookies=cookies,
-#                 auth=auth,
-#                 follow_redirects=follow_redirects,
-#                 timeout=timeout,
-#                 extensions=extensions
-#             )
+def get(base_url: str, endpoint: str, timeout: Timeout = DEFAULT_TIMEOUT, headers: Headers = DEFAULT_HEADERS)-> Response | None:
+    """A helper function to make a sync GET request using httpx,
+    and adding the endpoint parameter to the base url.
+    
+    example for endpoint paramater: /admin/orgs"""
+    try:
+        with Client(transport=SYNC_TRANSPORT, timeout=timeout) as client:
+            response = client.get(
+                url=base_url + endpoint,
+                headers=headers,
+            )
 
-#             return response
-#     except NetworkError:
-#         logger.error("network error while making the request")
-#         return None
-#     except HTTPStatusError as e:
-#         return e.response
-#     except RequestError as e:
-#         logger.error("Error while making a request", error=e)
-#         return None
+            return response
+    except NetworkError as e:
+        logger.error("network error while making the request", error=e)
+        return None
+    except HTTPStatusError as e:
+        return e.response
+    except RequestError as e:
+        logger.error("Error while making a request", error=e)
+        return None
+
+async def async_get(base_url: str, endpoint: str, timeout: Timeout = DEFAULT_TIMEOUT, headers: Headers = DEFAULT_HEADERS)-> Response | None:
+    """A helper function to make an async GET request using httpx,
+    and adding the endpoint parameter to the base url.
+    
+    example for endpoint paramater: /admin/orgs"""
+    try:
+        async with AsyncClient(transport=ASYNC_TRANSPORT, timeout=timeout) as client:
+            response = await client.get(
+                url=base_url + endpoint,
+                headers=headers,
+            )
+            return response
+    except NetworkError as e:
+        logger.error("network error while making the request", error=e)
+        return None
+    except HTTPStatusError as e:
+        return e.response
+    except RequestError as e:
+        logger.error("Error while making a request", error=e)
+        return None
+
