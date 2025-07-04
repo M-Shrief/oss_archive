181342c242243ff436a1aec00ec5573eb6c9168d
m-shrief <m.shrief.dev@proton.me>
2025-07-04 17:07:45 +0300

    Modified Forgejo's tasks to use httpx utils instead of the deleted requests module.

--- oss_archive/components/forgejo/tasks.py
+++ oss_archive/components/forgejo/tasks.py
@@ -1,17 +1,22 @@
 from httpx import Timeout
 ###
-from oss_archive.components.forgejo import requests
-from oss_archive.components.forgejo.schema import ForgejoRepo, MigrateRepoReqBody
+from oss_archive.config import Forgejo
+from oss_archive.utils import httpx
 from oss_archive.utils.logger import logger
+from oss_archive.components.forgejo.schema import ForgejoRepo, MigrateRepoReqBody
+
 
 
 async def migrate_repo_task(body: MigrateRepoReqBody):
     logger.info("Migrating repo...", repo=f"{body.repo_owner}/{body.repo_name}", source=body.clone_addr)
-    result = await requests.async_post(
+
+    result = await httpx.async_post(
+        base_url=Forgejo.get("base_url") or "",
         endpoint="/repos/migrate",
         body=body.model_dump(),
         timeout=Timeout(timeout=500.0, connect=100.0)
         )
+
     if result is None:
         logger.error("Migrating error", result="result is Noen")
         return
