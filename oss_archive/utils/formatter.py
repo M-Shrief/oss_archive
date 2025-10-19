f9f64b9df04d00286f0645f81f3edeb0c5aa78b8
m-shrief <m.shrief.dev@proton.me>
2025-10-19 23:21:29 +0300

    updated format_oss_fullname() to correspond to changes in our models & components.

--- oss_archive/utils/formatter.py
+++ oss_archive/utils/formatter.py
@@ -1,4 +1,4 @@
 
 
-def format_repo_fullname(owner_username: str, repo_name: str)->str:
+def format_oss_fullname(owner_username: str, repo_name: str)->str:
     return f"{owner_username}:{repo_name}"
