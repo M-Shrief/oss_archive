ab574903fdb2b0156df54a03e09e4e5462111945
m-shrief <m.shrief.dev@proton.me>
2025-10-19 23:53:52 +0300

    Commented out logger's pretty print config.

--- oss_archive/utils/logger.py
+++ oss_archive/utils/logger.py
@@ -1,28 +1,28 @@
 import logging
 import structlog
 ###
-from oss_archive.config import ENV
+# from oss_archive.config import ENV
 
 proccessors_configs = []
 
-if ENV == "dev": # Prettier logs for development
-    proccessors_configs = [
-        structlog.contextvars.merge_contextvars,
-        structlog.processors.add_log_level,
-        structlog.processors.StackInfoRenderer(),
-        structlog.dev.set_exc_info,
-        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-        structlog.dev.ConsoleRenderer(),
-    ]
-else: # JSON format, if it's in production
-    proccessors_configs = [
-        structlog.contextvars.merge_contextvars,
-        structlog.processors.add_log_level,
-        structlog.dev.set_exc_info,
-        structlog.processors.StackInfoRenderer(),
-        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-        structlog.processors.JSONRenderer(),
-    ]
+# if ENV == "dev": # Prettier logs for development
+#     proccessors_configs = [
+#         structlog.contextvars.merge_contextvars,
+#         structlog.processors.add_log_level,
+#         structlog.processors.StackInfoRenderer(),
+#         structlog.dev.set_exc_info,
+#         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+#         structlog.dev.ConsoleRenderer(),
+#     ]
+# else: # JSON format, if it's in production
+proccessors_configs = [
+    structlog.contextvars.merge_contextvars,
+    structlog.processors.add_log_level,
+    structlog.dev.set_exc_info,
+    structlog.processors.StackInfoRenderer(),
+    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+    structlog.processors.JSONRenderer(),
+]
 
 structlog.configure(
     processors=proccessors_configs,
