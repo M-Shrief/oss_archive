3e516c3b4c8603dc4c6ad22fc8bbfdefd77ffdc6
m-shrief <m.shrief.dev@proton.me>
2025-11-03 08:27:54 +0200

    Updated does_*_exists() to use optional paramaters for sync_db or async_db, rather than using a condition depending on the type of db.

--- oss_archive/seeders/helpers.py
+++ oss_archive/seeders/helpers.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select, exc 
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Any, Annotated
 from collections.abc import Sequence
 ### 
 from oss_archive.utils.logger import logger
@@ -33,69 +32,71 @@ async def get_all_owners(db: Session) -> Sequence[OwnerModel] | None:
         stmt = select(OwnerModel)
         res = db.scalars(stmt)
         owners = res.all()
-        logger.info("Got owners", length=len(owners))
         return owners
     except Exception as e:
         logger.error("Unknown error getting all owners", error=e)
         return None
 
 
-async def does_category_exists(category_key: str, db: AsyncSession |  Session) -> bool | None:
-    """check if category exists, if result is None then there was unknown error."""
+async def does_category_exists(category_key: str, async_db: AsyncSession | None = None, sync_db: Session | None = None) -> bool | None:
+    """check if category exists, if result is None then there was unknown error.
+    Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
-        if type(db) is AsyncSession:
-            stmt = select(CategoryModel).where(CategoryModel.key == category_key)
-            res = await db.scalars(statement=stmt)
-
+        stmt = select(CategoryModel).where(CategoryModel.key == category_key)
+        if async_db is not None:
+            res = await async_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
-        elif type(db) is Session:
-            stmt = select(CategoryModel).where(CategoryModel.key == category_key)
-            res = db.scalars(statement=stmt)
-
+        elif sync_db is not None:
+            res = sync_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
+        else:
+            return None
     except exc.NoResultFound:
         return False
-    except Exception:
+    except Exception as e:
+        logger.error("Error checking category existence", category_key=category_key, error=e)
         return None
 
-async def does_owner_exists(owner_username: str, db: AsyncSession |  Session) -> bool | None:
-    """check if owner exists, if result is None then there was unknown error."""
+async def does_owner_exists(owner_username: str, async_db: AsyncSession | None = None, sync_db: Session | None = None) -> bool | None:
+    """check if owner exists, if result is None then there was unknown error.
+    Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
-        if type(db) is AsyncSession:
-            stmt = select(OwnerModel).where(OwnerModel.username == owner_username)
-            res = await db.scalars(statement=stmt)
-
+        stmt = select(OwnerModel).where(OwnerModel.username == owner_username)
+        if async_db is not None:
+            res = await async_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
-        elif type(db) is Session:
-            stmt = select(OwnerModel).where(OwnerModel.username == owner_username)
-            res = db.scalars(statement=stmt)
-
+        elif sync_db is not None:
+            res = sync_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
+        else:
+            return None
     except exc.NoResultFound:
         return False
-    except Exception:
+    except Exception as e:
+        logger.error("Error checking owner existence", owner_username=owner_username, error=e)
         return None
 
-async def does_oss_exists(oss_fullname: str, db: AsyncSession |  Session) -> bool | None:
-    """check if OSS exists, if result is None then there was unknown error."""
+async def does_oss_exists(oss_fullname: str, async_db: AsyncSession | None = None, sync_db: Session | None = None) -> bool | None:
+    """check if OSS exists, if result is None then there was unknown error.
+    Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
-        if type(db) is AsyncSession:
-            stmt = select(OSSModel).where(OSSModel.fullname == oss_fullname)
-            res = await db.scalars(statement=stmt)
-
+        stmt = select(OSSModel).where(OSSModel.fullname == oss_fullname)
+        if async_db is not None:
+            res = await async_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
-        elif type(db) is Session:
-            stmt = select(OSSModel).where(OSSModel.fullname == oss_fullname)
-            res = db.scalars(statement=stmt)
-
+        elif sync_db is not None:
+            res = sync_db.scalars(statement=stmt)
             _ = res.one() # if it exists in won't raise an error
             return True
+        else:
+            return None
     except exc.NoResultFound:
         return False
-    except Exception:
+    except Exception as e:
+        logger.error("Error checking OSS existence", oss_fullname=oss_fullname, error=e)
         return None
